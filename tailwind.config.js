ba1bc5725af97e1b0c443c0242d0af162e8fd185
jesril <jesrilmnm@gmail.com>
2022-01-30 15:35:06 +0800

    Initialize

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#343434',
+        secondary: '#0B3D7C'
+      },
+      fontFamily:{
+        body: ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
